cf045c59539264b0ce3944eecd1171d414e32889
omdhameliya6409 <159802662+omdhameliya6409@users.noreply.github.com>
2024-02-12 21:42:58 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+var k=["RED", "AND", "WHITE"]
+
+for (var i=0;i<k.length;i++){
+    if(k[i]=="RED")
+    {
+        console.log(k[i])
+    }
+    else if(k[i]=="AND")
+    {
+        console.log(k[i].toString().toLocaleLowerCase())
+    }
+    else if(k[i]=="WHITE")
+    {
+        console.log(k[i])
+    }
+}
